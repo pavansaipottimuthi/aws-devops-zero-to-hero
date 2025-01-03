34c6c3a6be0a85d9d726b72dd795ea2682040ef2
pavansaipottimuthi <108636953+pavansaipottimuthi@users.noreply.github.com>
2025-01-03 18:11:14 -0500

    test

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-set -e
 
-# Pull the Docker image from Docker Hub
-docker pull pavan2827/sample-python-project
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 pavan2827/sample-python-project
